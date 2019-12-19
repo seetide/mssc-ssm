4cfe1a308980d9a4f23d57c4c65d48d5752ca2da
seetide <seetide@gmail.com>
2019-12-19 06:53:46 -0500

    commit

---
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,67 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder().amount(new BigDecimal("12.99")).build();
+    }
+
+    @Transactional
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        System.out.println("should be NEW");
+        System.out.println(savedPayment.getState());
+
+        StateMachine<PaymentState, PaymentEvent> sm = paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
+
+        System.out.println("Should be PRE_AUTH or PRE_AUTH_ERROR");
+        System.out.println(sm.getState().getId());
+
+        System.out.println(preAuthedPayment);
+    }
+
+    @Transactional
+    @Test
+    void auth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        StateMachine<PaymentState, PaymentEvent> sm = paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
+
+        if(sm.getState().getId().equals(PaymentEvent.PRE_AUTH_APPROVED)) {
+            Payment finalPayment = paymentRepository.getOne(preAuthedPayment.getId());
+
+            System.out.println(sm.getState().getId());
+            System.out.println(finalPayment);
+        }
+
+    }
+}
